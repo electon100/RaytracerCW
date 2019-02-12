97f6755a8ddb4a7b59f68d2ab4276f0906d668e8
Sibela Chinareva <sibelachinareva@gmail.com>
2019-02-12 16:55:30 +0000

    Lightning

--- raytracer/Source/skeleton.cpp
+++ raytracer/Source/skeleton.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <glm/glm.hpp>
-#include <SDL.h>
+#include "SDL.h"
 #include "SDLauxiliary.h"
 #include "TestModelH.h"
 #include <stdint.h>
+#include <math.h>
 
 using namespace std;
 using glm::vec3;
@@ -25,27 +26,25 @@ struct Intersection {
   int triangleIndex;
 };
 
-void Update();
+bool Update(vec4& cameraPos, vector<Triangle>& triangles, float &yaw);
 void Draw(screen* screen, float focalLength, vector<Triangle> triangles, vec4 cameraPos);
 bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection);
+vec3 DirectLight( const Intersection& i, vector<Triangle> triangles);
 
 int main( int argc, char* argv[] )
 {
 
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
 
-  float x = SCREEN_WIDTH/2;
-  float y = SCREEN_HEIGHT/2;
-  float z = -2.2f;
+  float z = -2.0f;
   float focalLength = SCREEN_WIDTH/2;
-  vec4 cameraPos( 0, 0, z, 1.0f);
+  vec4 cameraPos ( 0.0f, 0.0f, z, 1.0f);
 
   vector<Triangle> triangles;
   LoadTestModel(triangles);
-
-  while( NoQuitMessageSDL() )
+  float yaw = 0.0f;
+  while( Update(cameraPos, triangles, yaw) )
     {
-      Update();
       Draw(screen, focalLength, triangles, cameraPos);
       SDL_Renderframe(screen);
     }
@@ -71,18 +70,18 @@ void Draw(screen* screen, float focalLength, vector<Triangle> triangles, vec4 ca
     for (int x = 0; x < SCREEN_WIDTH; x++) {
       vec4 start = cameraPos;
       vec4 direction = vec4(x-SCREEN_WIDTH/2, y-SCREEN_HEIGHT/2, focalLength, 1.0f);
-        if (ClosestIntersection(start, direction, triangles, closestIntersection)) {
-          PutPixelSDL(screen, x, y, triangles[closestIntersection.triangleIndex].color);
-        }
-        else {
-          PutPixelSDL(screen, x, y, vec3(0, 0, 0));
-        }
+      if (ClosestIntersection(start, direction, triangles, closestIntersection)) {
+        PutPixelSDL(screen, x, y, DirectLight(closestIntersection, triangles));
+      }
+      else {
+        PutPixelSDL(screen, x, y, vec3(0, 0, 0));
+      }
     }
   }
 }
 
 /*Place updates of parameters here*/
-void Update()
+bool Update(vec4& cameraPos, vector<Triangle>& triangles, float& yaw)
 {
   static int t = SDL_GetTicks();
   /* Compute frame time */
@@ -92,12 +91,59 @@ void Update()
   /*Good idea to remove this*/
   std::cout << "Render time: " << dt << " ms." << std::endl;
   /* Update variables*/
+  vec4 a(glm::cos(yaw), 0.0f, glm::sin(yaw), 0.0f);
+  vec4 b(0.0f, 1.0f, 0.0f, 0.0f);
+  vec4 c(-glm::sin(yaw), 0.0f, glm::cos(yaw), 0.0f);
+  vec4 zs(0.0f, 0.0f, 0.0f, 1.0f);
+
+  mat4 R(a, b, c, zs);
+  mat4 RC = glm::inverse(R);
+  SDL_Event e;
+  while(SDL_PollEvent(&e))
+  {
+    if (e.type == SDL_QUIT)
+    {
+      return false;
+    }
+    else if (e.type == SDL_KEYDOWN)
+    {
+      int key_code = e.key.keysym.sym;
+      switch(key_code)
+      {
+        case SDLK_UP:
+          /* Move camera forwards */
+          cameraPos.z += 1.0f;
+          break;
+        case SDLK_DOWN:
+          /* Move camera backwards */
+          cameraPos.z -= 1.0f;
+          break;
+        case SDLK_LEFT:
+          /* Move camera left */
+          yaw += 0.1f;
+          cameraPos = cameraPos * R;
+          break;
+        case SDLK_RIGHT:
+          /* Move camera right */
+          yaw -= 0.1f;
+          cameraPos = cameraPos * RC;
+          break;
+        case SDLK_ESCAPE:
+          /* Move camera quit */
+          return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 /* Finds closest intersection*/
 bool ClosestIntersection(vec4 s, vec4 d, const vector<Triangle>& triangles, Intersection& closestIntersection) {
   float maximum = std::numeric_limits<float>::max();
   float closestDistance = maximum;
+
+  d = glm::normalize(d);
 
   for (size_t i = 0; i < triangles.size(); i++) {
     vec4 v0 = triangles[i].v0;
@@ -115,9 +161,9 @@ bool ClosestIntersection(vec4 s, vec4 d, const vector<Triangle>& triangles, Inte
     float u = x.y;
     float v = x.z;
 
-    vec4 ray = s + t*glm::normalize(d);
+    vec4 ray = s + t*d;
 
-    if (u > 0 && v > 0 && ((u + v) < 1) && t >= 0) {
+    if (u >= 0 && v >= 0 && ((u + v) <= 1) && t >= 0) {
       float currentDistance = glm::distance(ray, s);
       if (currentDistance < closestDistance) {
         closestDistance = currentDistance;
@@ -134,4 +180,17 @@ bool ClosestIntersection(vec4 s, vec4 d, const vector<Triangle>& triangles, Inte
   else {
     return false;
   }
+}
+
+vec3 DirectLight(const Intersection& i, vector<Triangle> triangles){
+  vec4 lightPos(0, -0.5, -0.7, 1.0);
+  vec3 lightColour = 14.f * vec3(1, 1, 1);
+  float r = glm::distance(i.position, lightPos);
+  float A = 4 * M_PI * r * r;
+  vec3 B = lightColour / A;
+  vec4 r_hat = glm::normalize(lightPos - i.position);
+  vec4 n = triangles[i.triangleIndex].normal;
+  vec3 D = B * max(glm::dot(n, r_hat), 0.0f);
+
+  return D;
 }
